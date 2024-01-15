3a6c4b03335e48347130c67ab03713948a1bf949
rsalakhov <r.salahov@spectrumlabs.fi>
2024-01-15 16:19:08 +0300

    update commands

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+INITIAL_PATH=/Users/ridel1e/Projects/cardano-token-handler/
+
+mkdir "$INITIAL_PATH/out"
+mkdir "$INITIAL_PATH/out/logos"
--- update-metadata.sh
+++ update-metadata.sh
@@ -1,7 +1,11 @@
-git -C /Users/ridel1e/Projects/cardano-token-handler/sources/cardano-token-registry/  pull origin master
-git -C /Users/ridel1e/Projects/cardano-token-handler/sources/cardano-tokens/  pull origin master
-rm -rf /Users/ridel1e/Projects/cardano-token-handler/out/logos/*
-node /Users/ridel1e/Projects/cardano-token-handler/index.js
-cp -r /Users/ridel1e/Projects/cardano-token-handler/out/logos/ /Users/ridel1e/Projects/cardano-token-handler/out/server/logos/
-cp /Users/ridel1e/Projects/cardano-token-handler/out/cardano-token-list.json /Users/ridel1e/Projects/cardano-token-handler/out/server/
+#!/bin/bash
+
+INITIAL_PATH=/Users/ridel1e/Projects/cardano-token-handler/
+
+git -C "$INITIAL_PATH/sources/cardano-token-registry/"  pull origin master
+git -C "$INITIAL_PATH/sources/cardano-tokens/"  pull origin master
+rm -rf "$INITIAL_PATH/out/logos/*"
+node "$INITIAL_PATH/index.js"
+cp -r "$INITIAL_PATH/out/logos/" /Users/ridel1e/Projects/cardano-token-handler/out/server/logos/
+cp "$INITIAL_PATH/out/cardano-token-list.json" /Users/ridel1e/Projects/cardano-token-handler/out/server/
 
